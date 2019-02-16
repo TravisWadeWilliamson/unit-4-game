7ada9a2f17eada38636b4c6a0e4ffa3ab42b1cf6
Travis Williamson <cthwgw1791@gmail.com>
2019-02-16 00:09:40 -0800

    revising JS

--- assets/logic.js
+++ assets/logic.js
@@ -4,72 +4,65 @@
 // When you click any crystal it should accumulate to total score
 // Matching the randomly generated number creates a win, going over that number creates a loss. Both wins and loss resets the random number to match and those of the individual crystals.
 
-//Wish list: Timer, animation on crystal click
+//Wish list: Timer, animation on crystal click/ start button
 
 
 $(document).ready(function () {
 
     // set the variables for wins, losses and my score. We can do this as one variable cos a win will register as 1 win and a loss will register as 1 loss
-    let counter = 0;
+    let targetNum;
     let wins = 0;
     let losses = 0;
-    let score = 0;
+    let score;
+    // create an array of the different crystals 
+
+    const crystalVariety = {
+        red: 0,
+        gold: 0,
+        yellow: 0,
+        green: 0,
+        blue: 0,
+        pink: 0,
+        purple: 0
+    }
 
     // changed this to create a target random number between 50 and 99
-    let targetNum = Math.floor(Math.random() * 50) + 50;
+    function start() {
+        // set targetNum to random number
+        targetNum = Math.floor(Math.random() * 50) + 50;
+        // set score to 0
+        score = 0;
+        // create the js to push the random number generator number to the num-to-match ID
+        $('#num-to-match').text(targetNum);
+        // Generate and assign a random number to each crystal
+        for (let color in crystalVariety) {
+            crystalVariety[color] = Math.ceil(Math.random() * 15);
+        }
+    };
 
-    // create the js to push the random number generator number to the num-to-match ID
-    $('#num-to-match').text(targetNum);
+    //when the win or loss is going to reset the target number and score should be reset 0
+    start();
+        
+    // create the logic to accrue the random number to the score when clicking on a crystal.
+    $('.crystal-img').on('click', function () {
+        const currId = $(this).attr('id');
+        console.log(this);
+        crystalVariety[currId];
+        console.log(currId, crystalVariety[currId]);
+
+        score += crystalVariety[currId];
+        console.log(score);
+
+
+    //if the score = the targetNum then we increase wins by one
+    if (score === targetNum) {
+        console.log('you win');
+    } else if (score > targetNum) {
+        console.log('you lose');
+    };
+    //if the score > the targetNun then we increase the losses by one
     
-
-    // create an array of the different crystals 
-    let crystalVariety = [{
-        name: 'red',
-        image: 'assets/images/theta-red.png',
-        value: 0
-    },
-    {
-        name: 'gold',
-        image: 'assets/images/theta-gold.png',
-        value: 0
-    },
-    {
-        name: 'yellow',
-        image: 'assets/images/theta-yellow.png',
-        value: 0
-    },
-    {
-        name: 'green',
-        image: 'assets/images/theta-green.png',
-        value: 0
-    },
-    {
-        name: 'blue',
-        image: 'assets/images/theta-blue.png',
-        value: 0
-    },
-    {
-        name: 'pink',
-        image: 'assets/images/theta-pink.png',
-        value: 0
-    },
-    {
-        name: 'purple',
-        image: 'assets/images/theta-purple.png',
-        value: 0
-    }
-    ]
-
-    // Generate and assign a random number to each crystal
-    for (let i = 0; i < crystalVariety.length; i++) {
-        let value = Math.ceil(Math.random() * 15);
-        console.log(value);
-    }
-// create the logic to accrue the random number to score when clicking on a crystal.
-$('.crystal-img').on('click', function () {
-    console.log($(this).attr('id'))
-
-})
+    })
 
 
 
