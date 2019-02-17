ac12797f107209fb77280de268f2527ba3763c22
Travis Williamson <cthwgw1791@gmail.com>
2019-02-17 04:56:26 -0800

    found some jQuery css attributes to try. Some worked, others not. Added to CSS and HTML as well as edited JS

--- assets/logic.js
+++ assets/logic.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
     var losses = 0;
     var wins = 0;
     var score = 0;
+    var netDiamonds = 0;
 
     //Create the start game function
     let restartAndStart = function () {
@@ -20,7 +21,7 @@ $(document).ready(function () {
         //Clear the individual crystals out for next round
         $(".crystals").empty();
 
-        // Create an array for the images,
+        // Create an array for the gif images,
         var images = [
             'assets/images/green-diamond.gif',
             'assets/images/pink-diamond.gif',
@@ -34,15 +35,17 @@ $(document).ready(function () {
         // Pushes the random number to match to the HTML
         $('#rnd-num-to-match').html("<h2>Number to Match: " + numberToMatch + "</h2>");
 
-        // Sets score wins losses to 
+        // Sets score to zero 
         score = 0;
         $('#score').html("<h2>Score: " + score + "</h2>");
 
+        // Increments wins
         wins;
-        $('#wins').html("<h2>" + wins + (wins === 1 ? ' Win' : ' Wins') + "</h2>");
+        $('#wins').html("<h4>You have collected " + wins + (wins === 1 ? ' diamond' : ' diamonds') + "</h4>");
 
+        // Increments losses
         losses;
-        $('#losses').html("<h2>" + losses + (losses === 1 ? ' Loss' : ' Losses') + "</h2>");
+        $('#losses').html("<h4>You have lost " + losses + (losses === 1 ? ' diamond' : ' diamonds') + "</h4>");
 
         // Creates a random number for each crystal between 1 and 15
         for (let i = 0; i < 4; i++) {
@@ -58,9 +61,15 @@ $(document).ready(function () {
             });
             //Add gifs to page
             crystal.css({
-                "background-image":"url('" + images[i] + "')",  
-                "bacground-size": "cover"             
+                "background-image": "url('" + images[i] + "')",
+                "bacground-size": "cover"
             });
+            // Tried adding style the crystals div via jQuery
+            $('.crystals').css('border', '3px solid turquoise');
+            $('.crystals').css('border-radius', '5px');
+
+            // Tried adding animation to the crystal divs via jQuery but it didn't work....
+            // $('crystal').animate({bottom: '200px', top: '200px', }, 500);
 
             // Puts the dynamically created crystal div INTO the parent crystals div
             $('.crystals').append(crystal);
